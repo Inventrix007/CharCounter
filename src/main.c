9dd0fe3fa5ba6e406bdc656c5d87be9476f86c72
Bineet Singh <bineetchadha@gmail.com>
2025-04-18 16:21:57 +0000

    Basic Count Functionality

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void print_help()
 {
@@ -9,6 +10,8 @@ void print_help()
 	printf("	-h, --help : Show this help message\n");
 	printf("	-v, --version : Show program version\n");
 }
+FILE* openFile(char* fileName);
+void charCount(char* fileName);
 
 int main(int argc, char *argv[]){
 	printf("Main Starts\n");
@@ -24,9 +27,53 @@ int main(int argc, char *argv[]){
 	}
 	if(strcmp(argv[1],"-v")== 0 || strcmp(argv[1], "--version")== 0){
 		printf("Program version is 1.0.0.0\n");
-		return 0;
+	}
+	if(strcmp(argv[1], "-f")== 0 || strcmp(argv[1], "--file")==0){
+		printf("The file name is %s\n", argv[2]);
+		charCount(argv[2]);
 	}
 
 	
 	return 0;
 }
+
+FILE* openFile(char* fileName)
+{
+	printf("Inside openFile function\n");
+	if(access(fileName, F_OK) != 0){
+		printf("file does not exist");
+		return 0;
+	}
+	FILE *file = fopen(fileName, "r");
+	if(file == NULL){
+		printf("Error: could not open file%s\n",fileName);
+	}
+	// Print file content to terminal
+	char ch;
+	while ((ch = fgetc(file)) !=EOF){
+		putchar(ch);
+	}
+	// rewind the file pointer so that returned file is ready to be reused
+	rewind(file);
+
+	return file;
+}
+
+void charCount(char* fileName)
+{
+	printf("Inside CharCount Function %s\n", fileName);
+	FILE* file = openFile(fileName);
+
+	//count characters in file
+	int counter = 0;
+	char ch;
+	while( (ch = fgetc(file)) != EOF)
+	{
+		counter++;
+	}
+	printf("File %s contains %d characters.\n",fileName, counter);
+	
+	fclose(file);
+}
+
+
