231aea1a055181b059c586b0886e53c6fcf29116
Bineet Singh <bineetchadha@gmail.com>
2025-04-19 13:09:49 +0000

    Word Count implementation

--- src/main.c
+++ src/main.c
@@ -10,11 +10,50 @@ struct flags{
 	char* inputFile;
 	int charFlag;
 };
-FILE* output;
 
-void print_help()
+typedef struct WordNode{
+	char* word;
+	int count;
+	struct WordNode* next;
+} WordNode;
+
+FILE* output;
+#define Table_Size 1000
+WordNode* hashTable[Table_Size];
+
+int hash(char* word){
+	unsigned long hash = 5381;
+	int c;
+	while(( c= (unsigned char)*word++)){
+		hash = ((hash << 5) + hash) + c;
+	}
+	return hash%Table_Size;
+}
+
+void insert_word(char* word){
+	int index = hash(word);
+	WordNode *node = hashTable[index];
+
+	while(node != NULL){
+		if(strcmp( node->word, word) == 0){
+			node->count++;
+			return;
+		}
+		node = node->next;
+	}
+	//insert word
+	node = malloc(sizeof(WordNode)); 
+	node->count = 1;
+	node->word = word;
+	node->next = NULL;
+	hashTable[index]= node;
+}
+
+void print_help(int error)
 {
-	fprintf(output,"Error Encountered!\n");
+	if(error){
+		fprintf(output,"Error Encountered!\n");	
+	}
 	fprintf(output,"Options:\n");
 	fprintf(output,"	-h, --help : Show this help message\n");
 	fprintf(output,"	-v, --version : Show program version\n");
@@ -23,28 +62,26 @@ void print_help()
 }
 
 FILE* openFile(char* fileName);
-void charCount(char* fileName);
+int fileHandler(struct flags flag);
 
 int main(int argc, char *argv[]){
 	printf("Main Starts\n");
 	printf("%s\n",argv[0]);
 	if(argc<2){
-		print_help();
+		print_help(1);
 		return 0;
 	}
 	struct flags flag;
 	output = stdout;
 	for(int i = 1; i < argc; i++){
 		if(strcmp(argv[i],"-h") == 0 || strcmp(argv[i],"--help") == 0){
-			print_help();
+			print_help(0);
 		}		
 	    else if(strcmp(argv[i],"-v")== 0 || strcmp(argv[i], "--version")== 0){
 			fprintf(output,"Program version is 1.0.0.1\n");
 		}
 		else if(strcmp(argv[i], "-f")== 0 || strcmp(argv[i], "--file")==0){
 			i++;
-			fprintf(output,"The file name is %s\n", argv[i]);
-			flag.charFlag =1;
 			flag.inputFile = argv[i];
 		}
 		else if(strcmp(argv[i], "-o")==0 || strcmp(argv[i], "--file")==0){
@@ -52,8 +89,14 @@ int main(int argc, char *argv[]){
 			flag.outputFlag = 1;
 			flag.outputFileName = argv[i];
 		}
+		else if(strcmp(argv[i], "-c") ==0){
+			flag.charFlag = 1;
+		}
+		else if(strcmp(argv[i], "-w") ==0){
+			flag.wordFlag = 1;
+		}
 		else{
-			print_help();
+			print_help(0);
 		}
 	}
 
@@ -61,14 +104,46 @@ int main(int argc, char *argv[]){
 		output = fopen(flag.outputFileName, "w");
 		fprintf(output,"output file is : %s", flag.outputFileName);
 	}
-	if(flag.charFlag){
-		charCount(flag.inputFile);
-	}
-	
-	fprintf(output, "hello from main.c");
+
+	fileHandler(flag);
+	//fprintf(output, "hello from main.c");
 	fclose(output);
 	return 0;
 }
+
+int fileHandler(struct flags flag){
+	fprintf(output,"Inside file handler Function %s\n", flag.inputFile);
+	FILE* file = openFile(flag.inputFile);
+
+	//count characters in file
+	int counter = 0;
+	int wordCounter =0;
+	char ch;
+	while( (ch = fgetc(file)) != EOF)
+	{
+		if(flag.charFlag){
+			counter++;	
+		}
+		if(flag.wordFlag){
+			if(((unsigned char)ch == ' ') || ((unsigned char) ch == '\n')){
+				wordCounter++;
+			}
+		}
+		if(counter < 8){
+			printf("ascii char of %c is %d and signed is %d\n", ch, (unsigned char) ch, ch);
+		}
+		
+	}
+	fprintf(output,"File %s contains %d characters.\n",flag.inputFile, counter);
+	fprintf(output,"File %s contains %d words.\n",flag.inputFile, wordCounter);
+	fclose(file);
+}
+
+//void wordCount(char* fileName){
+//	FILE* inputFile = openFile(fileName);
+//	char ch;
+//	while()		
+//}
 
 FILE* openFile(char* fileName)
 {
